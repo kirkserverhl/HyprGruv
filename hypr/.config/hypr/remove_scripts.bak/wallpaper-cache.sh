acf273d5d38a8311023e478bf7aa08e12614a0c1
kirkserverhl <kirkserverhl@gmail.com>
2025-03-01 06:03:49 -0500

    Delete hypr/.config/hypr directory
    Signed-off-by: kirkserverhl <kirkserverhl@gmail.com>

--- hypr/.config/hypr/remove_scripts.bak/wallpaper-cache.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-generated_versions="$HOME/.config/settings/cache/wallpaper-generated"
-rm $generated_versions/*
-echo ":: Wallpaper cache cleared"
-notify-send "Wallpaper cache cleared"
