641857b11c21a9f3ecb9d79e4d9005b6941953af
kirkserverhl <kirkserverhl@gmail.com>
2025-03-01 08:14:18 -0500

    Update setup.sh
    Signed-off-by: kirkserverhl <kirkserverhl@gmail.com>

--- setup/setup.sh
+++ setup/setup.sh
@@ -11,11 +11,11 @@ GRAY="\e[38;2;60;56;54m"     	# 3c3836 ##
 BOLD="\e[1m"                 	# Bold   ##
 clear #####################################
 
-declare -A checklist
-checklist=(
+ declare -A checklist
+ checklist=(
     [packages]=false
     [config]=false
-    [post]=false
+    [shell]=false
 )
 
 # Function to print status messages
@@ -47,8 +47,6 @@ log_error() {
             else
                 echo " ✖️ $section" >> "$checklist_file"
             fi
-        
-    # done
 
 
 # Display the checklist using tte beams
@@ -64,9 +62,6 @@ log_error() {
 
 
 ##### Section 1: Installing Packages #####
-# yay -Syu stow lsd-print-git figlet  --noconfirm
-# cp -f ~/.dotfiles/assets/hyrland.conf /.config/hypr
-# clear
 
 echo -e "\n  🫠   Welcome to Hyprland Gruvbox Installation !!   🚀
             Sit back and enjoy the ride !!   \n"  #| lsd-print
@@ -79,14 +74,11 @@ echo -e "\n  🫠   Welcome to Hyprland Gruvbox Installation !!   🚀
 	yay -Syu stow figlet  --noconfirm || log_error "Failed to install stow and figlet"
 	yay -Syu lsd-print-git --noconfirm || log_error "Failed to install lsd-print-git"
 	PACKAGES1=(
-            eza fastfetch figlet ghostty gsettings-qt gum hyprgraphics hyprlang hyprpaper hyprpolkitagent hyprutil hyprwayland-scanner imagemagick lsd-print-git neovim nwg-dock-hyprland nwg-drawer nwg-look pacseek python-pywal16 python-pywalfox python-terminaltexteffects qt5-base qt5-declarative qt5-x11extras qt5ct-kde qt6-base qt6-declarative qt6ct-kde starship stow xorg-xinit wlogout yazi xsettingsd zsh --noconfirm
+            eza fastfetch ghostty gsettings-qt gum hyprgraphics hyprlang hyprpaper hyprpolkitagent hyprutil hyprwayland-scanner imagemagick neovim nwg-dock-hyprland nwg-drawer nwg-look pacseek python-pywal16 python-pywalfox python-terminaltexteffects qt5-base qt5-declarative qt5-x11extras qt5ct-kde qt6-base qt6-declarative qt6ct-kde starship stow xorg-xinit wlogout yazi xsettingsd zsh --noconfirm
             )
      yay -S --noconfirm "${PACKAGES1[@]}"
-     checklist[packages]=true
-} || checklist[packages]=false
-clear
 
-# List of essential packages
+     # List of essential packages
 ESSENTIAL_PACKAGES=("eza" "fastfetch" "figlet" "ghostty" "gum" "hyprlang" "hyprpaper" "waypaper" "hyprpolkitagent" "hyprutils" "hyprwayland-scanner" "imagemagick" "lsd-print-git" "neovim" "nwg-dock-hyprland" "nwg-drawer" "nwg-look" "pacseek" "python-pywal16" "python-pywalfox" "python-terminaltexteffects" "qt5-base" "qt5-declarative" "qt5-x11extras" "qt5ct-kde" "qt6-base" "qt6-declarative" "qt6ct-kde" "starship" "stow" "xorg-xinit" "yazi" "xsettingsd" "wlogout" "zsh")
 
 # Check for missing packages
@@ -105,12 +97,28 @@ if [ ${#MISSING_PACKAGES[@]} -ne 0 ]; then
 else
     echo "All essential packages are already installed."
 fi
+     
+     checklist[packages]=true
+} || checklist[packages]=false
+clear
 
-# Proceed with the installation script
-echo "Continuing with the installation process..."
+ PACKAGES1=(
+           amd-ucode archlinux-xdg-menu ark aylurs-gtk-shell base-devel bluez bluez-utils bpytop btrfs-progs cliphist cmake cmatrix dolphin duf editorconfig-checker  efibootmgr expac fortune-mod fortune-mod-archlinux fzf gimp go gparted grimblast-git grub-theme-vimix gruvbox-plus-icon-theme gsettings-qt gst-plugin-pipewire gtk-engine-murrine haskell-colourista  hyprcursor hypridle hyprpicker hyprshade hyprutils kate konsole konsole-gruvbox kvantum less libpulse libva-intel-driver lsd neovim-lspconfig network-manager-applet obs-studio otf-font-awesome pacman-mirrorlist pavucontrol pipewire qt5-graphicaleffects ranger rofi-calc rofi-wayland sddm-sugar-candy-git smile syntax-highlighting tig timeshift tldr++ tmux tree-sitter ttf-nerd-fonts-symbols ttf-sharetech-mono-nerd waybar waypaper wireplumber wl-clipboard wl-clipboard-history-git wtf wtype xclip xdg-desktop-portal-gtk  xdg-desktop-portal-hyprland xdg-desktop-portal-kde xrainbow-git xf86-video-amdgpu xf86-video-ati xf86-video-nouveau xf86-video-vmware xorg-server xorg-wayland xorg-xhost xorg-xinit xsettingsd yazi zig zoxide zsh-autosuggestions-git zsh-syntax-highlighting          
+           )
+                yay S --noconfirm "${PACKAGES1[@]}"
 
 
-##### Section 2: Shell-Configuration  #####
+##### Section 2: Configuration  #####
+{
+    log_status "  🛠️   Applying base configurations..." | lsd-print
+        sudo rm -rf /usr/share/hypr && sudo rm -rf ~/.config/hypr && cd ~/.hyprgruv && stow .config home --adopt | { log_error "Failed to replace hyprgruv configuration and run stow"; exit 1; }
+        cd ~/.hyprgruv/setup   || { log_error "Failed to navigate to ~/scripts"; exit 1; }
+        ./config.sh || { log_error "Failed to run config.sh"; exit 1; }
+      checklist[config]=true
+} ||  checklist[config]=false
+clear
+
+##### Section 3: Shell-Config  #####
 {
 	log_status "󰯂  Running post-configuration scripts..."
         cd ~/.hyprgruv/setup || { log_error "Failed to navigate to ~/scripts"; exit 1; }
@@ -119,21 +127,51 @@ echo "Continuing with the installation process..."
 } || checklist[shell]=false
 clear
 
-# Display a final message:
-echo "🎉 All theming packages have been installed!"
-echo "Your system is now ready for the next phase."
-echo "Press Enter when you're ready to proceed..."
-read -r  # Wait for user to press Enter
+##### Section 4: Checklist #####
 
-# Notify the user about the delay
-echo "Opening the next installation script in 10 seconds..."
+#rm -f ~/config_check.sh
+print_checklist_tte
 
-# Wait for 10 seconds before launching the next script
-sleep 10
+echo -e "\n       Hyprland Gruvbox Installation is Complete !! 🫠
+        A list of common helpful keybinds is below:" | lsd-print
 
-# Launch the next script in a new terminal window.
-# Switch for ghostty
-kitty -hold -e "/.hyprgruv/setup/install.sh" &
+echo -e "  ⌨️  ▏ 󰖳 + ENTER             👻   Ghostty Terminal
+  ⌨️  ▏ 󰖳 + B                     Firefox
+  ⌨️  ▏ 󰖳 + F                     Krusader Browser
+  ⌨️  ▏ 󰖳 + N                     NeoVim
+  ⌨️  ▏ 󰖳 + Q                  󰅙   Close Window
+  ⌨️  ▏ 󰖳 + SPACE              󰀻   Rofi App Launcher
+  ⌨️  ▏ 󰖳 + CTRL + Q           󰗽   Logout 
+  ⌨️  ▏ 󰖳 + Mouse Left        🪟   Move Window"
 
-# Optionally exit this script
-exit 0
+echo -e "\n   Display Full list of keybinds with:  ⌨️  ▏ 󰖳 + SPACE
+   or left-click the gear icon    in the Waybar" | lsd-print
+echo -e " Restart is required to complete setup !!" | lsd-print
+echo -e "  1.   🥾    Reboot Now \n
+  2.   🔙    Rerun Installation \n
+  3.   🚀    Exit Installation \n"
+
+read -p " Enter your choice: " choice
+echo -e ""
+
+
+##### Check the user's input  #####
+case $choice in
+  1)
+        echo " Rebooting now..." | lsd-print
+        sudo reboot
+        ;;
+  2)
+        echo " Rerunning the script..."   | lsd-print
+        exec "$0"  # Reruns the current script
+        ;;
+  3)
+        echo " Exiting. System will not reboot."  | lsd-print
+        exit 0
+        ;;
+  *)
+        echo " Invalid input. Exiting without reboot."  | lsd-print
+        exit 0
+        ;;
+esac
+
